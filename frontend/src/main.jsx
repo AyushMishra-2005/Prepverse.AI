b5141d73420a3d18d3072e1da8dae718adec3e93
AyushMishraFeb2005 <ayushmishra9449@gmail.com>
2025-07-18 14:18:54 +0530

    Deployment Commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,7 @@ import { AuthProvider } from './context/AuthProvider.jsx'
 import { QuizProvider } from './context/QuizContext.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const CLIENT_ID = "373557072774-rrdq5hq418l7fp8jqkur0h4i10g15is8.apps.googleusercontent.com"
-
-
+const CLIENT_ID = "1076489462113-94c8nj5rsrcq52uq7aeji2uq2268qlci.apps.googleusercontent.com"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
