8da4090aa0e5e8fec04d06c5ac2f59d5290a0bfd
Ayush Kumar Singh <aayushsingh5526@gmail.com>
2025-09-06 21:04:17 +0530

    quiz page updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', 'sans-serif'],
+        inter: ['"Inter"', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
